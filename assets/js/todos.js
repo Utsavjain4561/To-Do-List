1fcf6a5637509a6045ca825a830ce5fd5956441a
Utsavjain4561 <utsavslife@gmail.com>
2020-03-21 15:25:28 +0530

    Deleting Todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,11 @@ $('li').click(function(){
 	
 	$(this).toggleClass("selected");
 });
+$('span').click(function(event){
+
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+
+});
