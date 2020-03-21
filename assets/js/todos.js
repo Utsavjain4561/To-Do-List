093afd9d3f6ebd63e42717f3ed8942cdf9b9f9b0
Utsavjain4561 <utsavslife@gmail.com>
2020-03-21 15:40:20 +0530

    Basic Functionality Done

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,24 @@
-$('li').click(function(){
+//check on/of specific todos
+$('ul').on('click','li',function(){
 	
 	$(this).toggleClass("selected");
 });
-$('span').click(function(event){
+// deleting the specific todo when X span is clicked 
+$('ul').on('click','span',function(event){
 
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
+	// disable events to bubble of in parents
 	event.stopPropagation();
 
+});
+
+$('input[type="text"]').keypress(function(event){
+	//grabbing the type of chracter input @[ENTER]
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$('ul').append('<li><span>X</span> '+todoText+'</li');
+	}
 });
